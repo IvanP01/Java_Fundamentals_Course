de68a7e190a4e7556295ac7e616b5e4a436c670c
Ivan Padureac <padureacion@gmail.com>
2021-03-16 22:02:06 +0200

    added o lot of * marks*

--- Test.java
+++ Test.java
@@ -2,8 +2,8 @@ public class Test {
 
      public static void main (String [ ]  args) {
 	 
-         System.out.println("Hello World!");
-		 System.out.println("Congrats!");
-		 System.out.println("Goodbye!");
+         System.out.println("***Hello World!");
+		 System.out.println("***Congrats!");
+		 System.out.println("***Goodbye!");
         }
     }
